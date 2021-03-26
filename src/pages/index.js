21cc53acf939175f7ee70d9f417b3c093fa33a5f
BrianArgel <orobouro566@gmail.com>
2021-03-26 17:06:18 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { Layout, SEO, Jumbo } from "../components"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Jumbo />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/thanks/">Go to thanks</Link>
+    <Link to="/canceled/">Go to canceled</Link>
+  </Layout>
+)
+
+export default IndexPage
